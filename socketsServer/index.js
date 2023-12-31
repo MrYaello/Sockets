9bfdef99d8e47496e6b29ec4e1c7a8b5666752ac
Yael Lozano Estrada <yaellozanoes@ciencias.unam.mx>
2023-12-31 13:03:47 -0600

    Primer commit

---
+++ socketsServer/index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+//import { createServer } from 'node:http';
+
+const app = express();
+const server = http.Server(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  }
+});
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(cors());
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: "Test",
+  });
+});
+
+io.on('connection', (socket) => {
+  console.log(`[+]: ${socket.id} user connected.`);
+
+  socket.on('disconnect', () => {
+    socket.disconnect();
+    console.log("[-]: A user disconnected.")
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
