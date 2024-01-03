ec87c8b4a0068802347db1853f38373b4c6f2f9f
Yael Lozano Estrada <yaellozanoes@ciencias.unam.mx>
2024-01-03 04:32:29 -0600

    Screens

---
+++ socketsClient/screens/Login.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect} from "react";
+import { 
+  FormControl,
+  Box,
+  FormControlLabel,
+  FormControlLabelText, 
+  InputField, 
+  Input, 
+  FormControlHelper, 
+  FormControlHelperText, 
+  FormControlError, 
+  FormControlErrorText, 
+  FormControlErrorIcon, 
+  AlertCircleIcon,
+  ArrowRightIcon, 
+  ButtonText,
+  Button,
+  ButtonIcon } from "@gluestack-ui/themed";
+import { SafeAreaView, Text } from "react-native";
+
+const Login = ({ navigation }) => {
+  const [username, setUsername] = useState("");
+  const [isInvalid, setInvalid] = useState(false);
+  var invalid;
+  //const [password, setPassword] = useState("");
+
+  const handleSignIn = () => {
+    if (username.trim().length >= 8) {
+      console.log(username);
+    } else {
+      setInvalid(true);
+    }
+  }
+
+  return (
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: "#EEF1FF",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 12,
+      width: "100%",
+    }}>
+      <Box 
+        h="$32"
+        w="$72"
+      >
+        <Text style={{
+          alignSelf: "center",
+          fontSize: 26,
+          marginBottom: 10,
+        }}>Sign in </Text>
+        <FormControl
+          size="lg"
+          isDisabled={false}
+          isInvalid={isInvalid}
+          isReadOnly={false}
+          isRequiered={true}
+        >
+          <FormControlLabel mb="$1">
+            <FormControlLabelText>Username</FormControlLabelText>
+          </FormControlLabel>
+          <Input>
+            <InputField 
+              autoCorrect={false} 
+              type="text" 
+              defaultValue="" 
+              placeholder="Enter your username."
+              onChangeText={(value) => {
+                setUsername(value);
+                setInvalid(false);
+              }}  
+            />
+          </Input>
+          <FormControlHelper>
+            <FormControlHelperText>Must be at least 8 characters.</FormControlHelperText>
+          </FormControlHelper>
+          <FormControlError>
+            <FormControlErrorIcon as={AlertCircleIcon}/>
+            <FormControlErrorText>At least 8 characters requiered.</FormControlErrorText>
+          </FormControlError>
+        </FormControl>
+        <FormControl mt="$2">
+          <Button 
+            flexDirection="row"
+            justifyContent="space-between"
+            onPress={handleSignIn}
+          >
+            <ButtonText 
+              fontSize="$sm" 
+              fontWeight="$medium">
+              Get Started
+            </ButtonText>
+            <ButtonIcon as={ArrowRightIcon}/>
+          </Button>
+        </FormControl>
+      </Box>
+    </SafeAreaView>
+  )
+}
+
+
+
+export default Login;
--- socketsServer/index.js
+++ socketsServer/index.js
@@ -1,30 +1,20 @@
 import express from 'express';
-import cors from 'cors';
 import http from 'http';
 import { Server } from 'socket.io';
 //import { createServer } from 'node:http';
 
 const app = express();
 const server = http.Server(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-  }
-});
+const io = new Server(server);
 const PORT = 4000;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(cors());
 
 app.get('/api', (req, res) => {
   res.json({
     message: "Test",
   });
-});
-
-server.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
 });
 
 io.on("connection", (socket) => {
@@ -35,7 +25,11 @@ io.on("connection", (socket) => {
   })
 
   socket.on("disconnect", () => {
+    console.log(`[-]: ${socket.id} user disconnected.`)
     socket.disconnect();
-    console.log("[-]: A user disconnected.")
   })
 })
+
+server.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
