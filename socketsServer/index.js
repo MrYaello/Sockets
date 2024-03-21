c5650c8cca05a2b0fa528d820fe118145eed1a04
Yael Lozano Estrada <yaellozanoes@ciencias.unam.mx>
2024-03-21 00:46:59 -0600

    SQL Connect to createPool

--- socketsServer/index.js
+++ socketsServer/index.js
@@ -8,6 +8,7 @@ import handlebars from 'handlebars';
 import nodemailer from 'nodemailer';
 import credentialsSMTP from './credentialsSMTP.js';
 import credentialsSQL from './credentialsSQL.js';
+import e from 'express';
 
 //SQL Connection.
 const pool = mysql.createPool(credentialsSQL);
@@ -48,125 +49,98 @@ app.use(express.json());
 app.use('/uploads', express.static('uploads'))
 
 app.get('/', (req, res) => {
-  res.json(chatRooms);
+  res.send("¡Hello world!");
 });
 
-io.on("connection", (socket) => {
-  let user_id;
-  console.log(`[+]: ${socket.id} user connected.`);
+export const queryExecute = (sql, params, env, callback) => {
   pool.getConnection((err, connection) => {
-    if (err) throw err;
-    connection.query("SELECT c.id AS 'index', c.name, c.avatar, m.content AS 'state' FROM chatgroup c LEFT JOIN (SELECT * FROM message m1 WHERE (m1.recipient_id, m1.postDate) IN (SELECT recipient_id, MAX(postDate) FROM message GROUP BY recipient_id)) m ON c.id = m.recipient_id", (err, result) => {
-      if (err) console.log(err);
-      socket.emit("requestUsers", result);
-    });
-
-    socket.on("requestUsers", (online, id) => {
-      let query = "SELECT c.id AS 'index', c.name, c.avatar, m.content AS 'state' FROM chatgroup c LEFT JOIN (SELECT * FROM message m1 WHERE (m1.recipient_id, m1.postDate) IN (SELECT recipient_id, MAX(postDate) FROM message GROUP BY recipient_id)) m ON c.id = m.recipient_id WHERE c.id != ?";
-      connection.query(query, [id], (err, result) => {
-        if (err) console.log(err);
-        socket.emit("requestUsers", result);
-      });
-    });
-
-    socket.on("validateUsername", (auth) => {
-      let query = "SELECT id, salt FROM user WHERE username=? OR email=? OR phonenumber=?"; 
-      connection.query(query, [auth, auth, auth], (err, result) => {
-        if (err) console.log(err);
-        socket.emit("validateUsername", result);
-      });
-    });
-
-    socket.on("login", (username, password) => {
-      let query = "SELECT username, id AS \"index\" FROM user WHERE username=? AND password=? OR email=? AND password=? OR phonenumber=? AND password=?";
-      connection.query(query, [username, password, username, password, username, password], (err, result) => {
-        if (err) console.log(err);
-        socket.emit("login", result);
-      });
-    });
-
-    socket.on("register", (username, salt, password, email, phonenumber) => {
-      let queryRegister = "INSERT INTO user (username, salt, password, email, phonenumber) VALUES (?,?,?,?,?)";
-      connection.query(queryRegister, [username, salt, password, email, phonenumber], (err, register) => {
-        if (err) console.log(err);
-        else {
-          connection.query("INSERT INTO chatgroup (name) VALUES (?)", [username], (err, group) => {
-            if (err) console.log(err);
-            else {
-              connection.query("INSERT INTO user_chatgroup VALUES (?,?)", [register.insertId, group.insertId], (err, result) => {
-                if (err) console.log(err);
-              });
-            }
-          });
-        }
-      });
-    });
-
-    socket.on("getAvatarSource", (username) => {
-      let query = "SELECT avatar FROM chatgroup WHERE name=?"
-      connection.query(query, [username], (err, result) =>{
-        if (err) console.error(err);
-        if (result.length > 0) {
-          const avatarSource = result[0].avatar;
-          socket.emit("getAvatarSource", avatarSource);
-        } else {
-          socket.emit("getAvatarSource", null);
-        }
-      });
-    });
-
-    socket.on("sendVerificationEmail", (email) => {
-      let deletePrevious = "DELETE FROM verification WHERE email=?";
-      connection.query(deletePrevious, [email], (err, result) => {
-        if (err) console.log(err);
-      });
-
-      let code = Math.floor(Math.random() * (9999)).toString().padStart(4, "0");
-      let query = "INSERT INTO verification (email, code) VALUES (?, ?)"
-      connection.query(query, [email, code], (err, result) => {
-        if (err) console.log(err);
-      });
-      sendMail(code, email);
-    });
-
-    socket.on("verifyEmailCode", (email) => {
-      let query = "SELECT code FROM verification WHERE DATE_SUB(CURDATE(), INTERVAL 30 MINUTE) <= created AND email=?";
-      connection.query(query, [email], (err, result) => {
-        if (err) console.log(err);
-        if (result.length > 0) {
-          const code = result[0].code;
-          socket.emit("verifyEmailCode", code);
-        } else {
-          socket.emit("verifyEmailCode", null);
-        }
-      });
-    });
-
-    socket.on("identify", (id) => {
-      user_id = id;
-    }); 
-
-    socket.on("disconnect", () => {
-      console.log(`[-]: ${socket.id} user disconnected.`)
-      socket.disconnect();
-    });
-
-    socket.on("sendMessage", (sender_id, recipient_id, content) => {
-      let query = "INSERT INTO message VALUES (DEFAULT, ?, ?, DEFAULT, ?)";
-      connection.query(query, [sender_id, recipient_id, content], (err, result) => {
-        if (err) console.log(err);
-        socket.emit("sendMessage", result);
-      });
-    });
-
-    socket.on("deployMessages", (group_id) => {
-      let query = "SELECT * FROM message WHERE recipient_id = ?";
-      connection.query(query, [group_id], (err, result) => {
-        if (err) console.log(err);
-        socket.emit("deployMessages", result);
-      })
+    if (err) {
+      console.log(err);
+      return;
+    }
+    connection.query(sql, params, (err, result, field) => {
+      callback(env, result, err, field);
+      connection.release();
     });
   });
+}
+
+io.on("connection", (socket) => {
+  const emitResultSocket = (env, result, err, field) => {
+    if (err) console.log(err);
+    socket.emit(env, result);
+  }
+
+  let user_id;
+  console.log(`[+]: ${socket.id} user connected.`);
+  let queryUpdateUsers = "SELECT c.id AS 'index', c.name, c.avatar, m.content AS 'state' FROM chatgroup c LEFT JOIN (SELECT * FROM message m1 WHERE (m1.recipient_id, m1.postDate) IN (SELECT recipient_id, MAX(postDate) FROM message GROUP BY recipient_id)) m ON c.id = m.recipient_id"; 
+  queryExecute(queryUpdateUsers, [], "requestUsers", emitResultSocket);
+
+  socket.on("requestUsers", (online, id) => {
+    let query = "SELECT c.id AS 'index', c.name, c.avatar, m.content AS 'state' FROM chatgroup c LEFT JOIN (SELECT * FROM message m1 WHERE (m1.recipient_id, m1.postDate) IN (SELECT recipient_id, MAX(postDate) FROM message GROUP BY recipient_id)) m ON c.id = m.recipient_id WHERE c.id != ?";
+    queryExecute(query, [id], "requestUsers", emitResultSocket);
+  });
+
+  socket.on("validateUsername", (auth) => {
+    let query = "SELECT id, salt FROM user WHERE username=? OR email=? OR phonenumber=?"; 
+    queryExecute(query, [auth, auth, auth], "validateUsername", emitResultSocket);
+  });
+
+  socket.on("login", (username, password) => {
+    let query = "SELECT username, id AS \"index\" FROM user WHERE username=? AND password=? OR email=? AND password=? OR phonenumber=? AND password=?";
+    queryExecute(query, [username, password, username, password, username, password], "login", emitResultSocket);
+  });
+
+  socket.on("register", (username, salt, password, email, phonenumber) => {
+    let queryRegister = "INSERT INTO user (username, salt, password, email, phonenumber) VALUES (?,?,?,?,?)";
+    queryExecute(queryRegister, [username, salt, password, email, phonenumber]);
+  });
+
+  socket.on("getAvatarSource", (username) => {
+    let query = "SELECT avatar FROM chatgroup WHERE name=?";
+    queryExecute(query, [username], "getAvatarSource", emitResultSocket);
+  });
+
+  socket.on("sendVerificationEmail", (email) => {
+    let deletePrevious = "DELETE FROM verification WHERE email=?";
+    queryExecute(deletePrevious, [email]);
+
+    let code = Math.floor(Math.random() * (9999)).toString().padStart(4, "0");
+    let query = "INSERT INTO verification (email, code) VALUES (?, ?)"
+    queryExecute(query, [email, code]);
+    sendMail(code, email);
+  });
+
+  socket.on("verifyEmailCode", (email) => {
+    let query = "SELECT code FROM verification WHERE DATE_SUB(CURDATE(), INTERVAL 30 MINUTE) <= created AND email=?";
+    queryExecute(query, [email], "verifyEmailCode", emitResultSocket);
+  });
+
+  socket.on("identify", (id) => {
+    user_id = id;
+  }); 
+
+  socket.on("disconnect", () => {
+    console.log(`[-]: ${socket.id} user disconnected.`)
+    socket.disconnect();
+  });
+  /*
+  socket.on("sendMessage", (sender_id, recipient_id, content) => {
+    let query = "INSERT INTO message VALUES (DEFAULT, ?, ?, DEFAULT, ?)";
+    connection.query(query, [sender_id, recipient_id, content], (err, result) => {
+      if (err) console.log(err);
+      socket.emit("sendMessage", result);
+    });
+  });
+
+  socket.on("deployMessages", (group_id) => {
+    let query = "SELECT * FROM message WHERE recipient_id = ?";
+    connection.query(query, [group_id], (err, result) => {
+      if (err) console.log(err);
+      socket.emit("deployMessages", result);
+    })
+  });
+  */
 });
 
 server.listen(PORT, () => {
